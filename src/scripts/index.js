430857d5a225a9e01614eccaceb0a4f031c0e4a0
Sauloviniciust <sauloviniciust@gmail.com>
2024-09-26 03:04:51 -0300

    criando

---
+++ src/scripts/index.js
@@ -0,0 +1,39 @@
+function abrirRotas() {
+    // Base da URL
+    const urlBase = "https://envios.adminml.com/logistics/monitoring-distribution/detail/";
+
+    // Captura o valor da caixa de entrada
+    const rotasInput = document.getElementById("rotas").value;
+
+    // Divide os números de rota por linha
+    const rotas = rotasInput.split("\n");
+
+    // Abrir cada rota imediatamente sem delay
+    rotas.forEach(function(rota) {
+        const urlCompleta = urlBase + rota.trim(); 
+        if (rota.trim()) {
+            // Abre cada URL em uma nova aba
+            window.open(urlCompleta, '_blank');
+        }
+    });
+}
+
+function abrirPacotes() {
+    // Base da URL
+    const urlBase = "https://envios.adminml.com/logistics/package-management/package/";
+
+    // Captura o valor da caixa de entrada
+    const pacotesInput = document.getElementById("pacotes").value;
+
+    // Divide os números de pacotes por linha
+    const pacotes = pacotesInput.split("\n");
+
+    // Abrir cada pacote imediatamente sem delay
+    pacotes.forEach(function(pacote) {
+        const urlCompleta = urlBase + pacote.trim();
+        if (pacote.trim()) {
+            // Abre cada URL em uma nova aba
+            window.open(urlCompleta, '_blank');
+        }
+    });
+}
